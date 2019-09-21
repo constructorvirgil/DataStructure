6c6f968c01963192d5988b0305326ed906e11183
constructorvirgil <38716951+constructorvirgil@users.noreply.github.com>
2019-09-21 18:07:02 +0800

    Add files via upload

---
+++ BTree.cpp
@@ -0,0 +1,149 @@
+﻿#include <iostream>
+#include <fstream>
+#define TREESIZE 10000
+
+using namespace std;
+struct Node
+{
+	Node* leftChild;
+	Node* rightChild;
+	int val;
+};
+
+struct BTree
+{
+	Node* root;
+
+};
+
+BTree bTree;
+int vals[TREESIZE] = { 0 };
+
+void CreateRecur(Node* node,int j)
+{
+	if (vals[j * 2 + 1] != 0)
+	{
+		node->leftChild = new Node();
+		node->leftChild->val = vals[j * 2 + 1];
+		CreateRecur(node->leftChild, j * 2 + 1);
+	}
+	if (vals[j * 2 + 2] != 0)
+	{
+		node->rightChild = new Node();
+		node->rightChild->val = vals[j * 2 + 2];
+		CreateRecur(node->rightChild, j * 2 + 2);
+	}
+	return ;
+}
+
+int CreateTreeFromFile()
+{
+	if (bTree.root != nullptr)
+	{
+		cout << "Tree Existed" << endl;
+		return -1;
+	}
+	ifstream filein("data.txt");
+	if (filein.is_open() != true)
+	{
+		cout << "Open File Failed" << endl;
+		return -2;
+	}
+
+	int d;
+
+	int i = 0;
+	while (1)
+	{
+		filein >> d;
+		if (d == 0)
+		{
+			break;
+		}
+		vals[i] = d;
+		i++;
+	}
+
+	bTree.root = new Node();
+	Node* node = bTree.root;
+	node->val = vals[0];
+
+	CreateRecur(node,0);
+
+	filein.close();
+	return 0;
+}
+
+void PrePrintTree(Node * node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+	cout << node->val << endl;
+	if (node->leftChild != nullptr)
+	{
+		PrePrintTree(node->leftChild);
+	}
+	if (node->rightChild != nullptr)
+	{
+		PrePrintTree(node->rightChild);
+	}
+	return;
+}
+
+void MidPrintTree(Node* node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+	if (node->leftChild != nullptr)
+	{
+		MidPrintTree(node->leftChild);
+	}
+	cout << node->val << endl;
+	if (node->rightChild != nullptr)
+	{
+		MidPrintTree(node->rightChild);
+	}
+
+	return;
+}
+
+void LastPrintTree(Node* node)
+{
+	if (node == nullptr)
+	{
+		return;
+	}
+	if (node->leftChild != nullptr)
+	{
+		LastPrintTree(node->leftChild);
+	}
+	if (node->rightChild != nullptr)
+	{
+		LastPrintTree(node->rightChild);
+	}
+	cout << node->val << endl;
+
+	return;
+}
+
+
+int main()
+{
+	if (CreateTreeFromFile() < 0)
+	{
+		cout << "Create Tree Failed" << endl;
+		return -1;
+	}
+	cout << "先序遍历二叉树" << endl;
+	PrePrintTree(bTree.root);
+	cout << "中序遍历二叉树" << endl;
+	MidPrintTree(bTree.root);
+	cout << "后序遍历二叉树" << endl;
+	LastPrintTree(bTree.root);
+
+	return 0;
+}
---
+++ LinkedList.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+//结点
+struct Node
+{
+	int val;
+	Node* pre;
+	Node* next;
+};
+
+struct  List
+{
+	Node * head;//链表的头结点
+	int length;//表中结点的个数
+};
+
+List list;
+
+//通过读取文件来创建一个新的链表
+int CreateListFromFile()
+{
+	if (list.head != nullptr)
+	{
+		cout << "List Existed" << endl;
+		return -1;
+	}
+	ifstream filein("ListData.txt");
+	if (filein.is_open() != true)
+	{
+		cout << "Open File Failed" << endl;
+		return -2;
+	}
+	list.head = new Node();
+	list.length = 1;
+	filein >> list.head->val;
+	Node * preNode = list.head;
+	Node * nextNode = list.head;
+	int d;
+	while (1)
+	{
+		filein >> d;
+		if(d == 0)
+		{
+			break;
+		}
+		preNode = new Node();
+		list.length++;
+		preNode->val = d;
+		preNode->pre = nextNode;
+		nextNode->next = preNode;
+		nextNode = preNode;
+
+	}
+
+	filein.close();
+	return 0;
+}
+
+//通过循环的方式打印链表
+int PrintList()
+{
+	if (list.head == nullptr)
+	{
+		cout << "Bad List" << endl;
+		return -1;
+	}
+	Node* node = list.head;
+	while (node != nullptr)
+	{
+		cout << node->val << " ";
+		node = node->next;
+	}
+	cout << endl;
+
+	return 0;
+}
+
+//通过递归的方式打印链表
+int PrintListRecur(Node * node)
+{
+	if (node  == nullptr)
+	{
+		cout << endl;
+		return 0;
+	}
+	cout << node->val << " ";
+	PrintListRecur(node->next);
+}
+
+//清空链表的所有内容
+int ClearList()
+{
+	Node * preNode = list.head->next;
+	Node * nextNode = list.head->next;
+	delete(list.head);
+	list.head = nullptr;
+	while (nextNode != nullptr)
+	{
+		preNode = nextNode->next;
+		delete(nextNode);
+		nextNode = preNode;
+	}
+	return 0;
+}
+
+int main()
+{
+	if (CreateListFromFile() < 0)
+	{
+		cout << "Create Linked List Failed" << endl;
+		return -1;
+	}
+	PrintList();
+	PrintListRecur(list.head);
+	ClearList();
+
+	return 0;
+}
